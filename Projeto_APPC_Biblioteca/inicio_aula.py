6e5f97f7663000fde63248744862b5117f324597
cissa-eduarda <ciceraeduardac@gmail.com>
2023-05-09 08:48:56 -0300

    Adicao de sistema de Busca

--- Projeto_APPC_Biblioteca/inicio_aula.py
+++ Projeto_APPC_Biblioteca/inicio_aula.py
@@ -1,48 +1,170 @@
-#Entrada de informacoes
-print('Sistema de livraria')
-dic = {}
-n=1
-conf = 0
-while n == 1:
+#Ferramentas
 
-    opc = int(input('digite 1 para cadastrar um livro, 2 para buscar e 3 para sair: '))
+d_livros={}
+l_autores=[]
+print('Sistema de Livraria')
 
-    if opc < 1 or opc > 3:
-        print("Opcao invalida, escolha uma disponivel!")
-    elif opc == 1:
-        while conf == 0:
-            lista = []
-            lista_autores = []
-            print('Insira as informacoes do livro')
-            cod = int(input('digite o codigo do livro: '))
-            if cod == 0:
-                a = int(input('digite 1 para parar e 2 para continar'))
-                if a == 1:
-                    break
-            ti = input('digite o titulo: ')
-            if ti == 0:
-                a = int(input('digite 1 para parar e 2 para continar'))
-                if a == 1:
-                    break
-            nautor = int(input('digite o numero de autores: '))
 
-            for autor in range(nautor):
-                autor = input(f'Digite o nome do {autor} autor:')
-                lista_autores.append(autor)
-            if nautor == 0:
-                a = int(input('digite 1 para parar e 2 para continar'))
-                if a == 1:
-                    break
-            pre = float(input('digite o preço do livro: '))
-            if pre == 0:
-                a = int(input('digite 1 para parar e 2 para continar'))
-                if a == 1:
-                    break
-            lista.append(cod)
-            lista.append(ti)
-            lista.append(nautor)
-            lista.append([lista_autores])
-            lista.append(pre)
+#Sistema de cadastro e Sistema de busca
+while True:
 
-        #correcao de informacoes
-        #validacoes variaveis
+    while True:
+        try:
+            escolha_sistema = int(input("Escolha entre as opcoes abaixo:\n 1 - Cadastrar livros \n 2 - Buscar livros \n 3 - Sair \n Digite a opção escolhida: "))
+            break
+        except ValueError:
+            print("\n\nDigite somente numeros!\n\n")
+
+    if escolha_sistema<1 or escolha_sistema>3:
+        print("\nEscolha um opçaõ valida!\n") 
+    else:
+        break
+           
+# Escolha do sistema 
+
+while 1<=escolha_sistema<=3:
+
+    ##Sai do sistema 
+  
+    if escolha_sistema ==3:
+        print("Volte sempre!")
+        break
+
+    #Sistema de Castro
+    if escolha_sistema ==1:
+        print("\n\n\tSistema de Cadastro")
+
+        deseja_continuar=1
+        contador = 0
+
+        while deseja_continuar==1:
+            contador+=1
+
+            #Entrada e validacao codigo_livro
+            while True: 
+                try:
+                    codigo_livro= int(input(f"\nDigite o codigo do livro {contador}: "))
+                    break
+
+                except ValueError:
+                    print("Digite uma entrada valida! Apenas numeros inteiros")
+
+            ##Busca do codigo para ver se o mesmo já existe
+            if codigo_livro in d_livros.keys():
+                print(f"Codigo ja existente! Pertence ao livro {d_livros[0]}")
+                codigo_livro= int(input(f"Digite novamente o codigo do livro {contador}: "))
+
+            #Entrada nome do livro
+            titulo_livro = input(f"Digite o nome do livro {contador}: ").upper()
+
+            #Entrada numero de autores
+            while True:
+                try:
+                    n_autores = int(input(f"Digite o numero de autores do livro {contador}: "))
+                    break
+
+                except ValueError:
+                    print("Digite apenas numeros!")
+            
+            for autor in range(1,n_autores+1):
+                autor=input(f"Digite nome do {autor}º autor do livro {contador}: ")
+                l_autores.append(autor)
+            
+            #Preco
+            preco_livro= float(input(f"Insira o valor do preco do livro {contador}: "))
+
+            #Conferencia de informacoes
+
+            while True:
+                try:
+                    print(f"Informacoes adicionadas: \n 1- Codio Livro: {codigo_livro} \n 2- Titulo: {titulo_livro} \n 3- Numero de Autores: {n_autores} \n 4- Nome Autores: {l_autores} \n 5 -Preco: {preco_livro}")
+                    conf_informacoes = int(input(f"As informações estao corretas? [1-SIM] ou [2-NAO]: "))
+                   
+                    if conf_informacoes == 1:
+                        break
+                    elif conf_informacoes > 6 or conf_informacoes< 1:
+                        print("Escolha uma opçao valida!")
+                    else:
+                        n_alteracao = int(input("Escolha o item para ser alterada ou [6]- Para sair : "))
+                        if n_alteracao==6:
+                            break
+                        elif n_alteracao ==1:
+                            codigo_livro=int(input("Insira o codigo do livro correto: "))
+                        elif n_alteracao ==2:
+                            titulo_livro= input("Digite o titulo correto do livro: ")
+                        elif n_alteracao ==3:
+                            n_autores = int(input("Digite o numero de autores correto: "))
+                        elif n_alteracao==4:
+                            for autor in range(1,n_autores+1):
+                                autor=input(f"Digite nome do {autor}º autor: ")
+                                l_autores.append(autor)
+                        elif n_alteracao ==5:
+                            preco_livro= float(input(f"Insira o preco do livro {contador}, correto: "))
+                except ValueError:
+                    print("Insira apenas numeros inteiros!")
+            
+             #Adicao dicionario
+            d_livros[codigo_livro]=[titulo_livro,n_autores,l_autores]
+            print(f"Livro adicionado com sucesso! {d_livros}")
+            
+            #Continuacao Cadastro
+
+            deseja_continuar=(input("Deseja Inserir outro livro?: [1-Sim / 2-Nao]: "))
+
+            #Proximo programa colocar em laco antes de tudo
+            if deseja_continuar==2:
+                while True:
+                    while True:
+                        try:
+                            escolha_sistema = int(input("Escolha entre as opcoes abaixo:\n 1 - Cadastrar livros \n 2 - Buscar livros \n 3 - Sair \n Digite a opção escolhida: "))
+                            break
+                        except ValueError:
+                            print("\n\nDigite somente numeros!\n\n")
+
+                    if escolha_sistema<1 or escolha_sistema>3:
+                        print("\nEscolha uma opçaõ valida!\n") 
+                    else:
+                        break
+            
+
+
+     #Sistema de Busca
+
+    if escolha_sistema==2:
+
+        print("\n\n \t Sistema de Busca! ")
+        print(f"Livros cadastrados no Sistema {contador}")
+
+        #Escolha do tipo de busca desejado
+
+        while True:
+
+            while True:
+                try:
+                    busca=input(f"Deseja buscar por: [1] - Titulo [2] - Codigo [3] - Sair")
+                    break
+                except ValueError:
+                    print("Digite apenas numeros!")
+            if busca<1 or busca>3:
+                print("Escolha uma opcao valida!")
+            else:
+                break
+        
+        
+
+
+        busca_titulo=input("Digite o nome do livro para Busca: ").upper().trim()
+
+
+        #Deseja buscar novamente outro livro?
+           
+
+            
+
+
+
+
+#Entrada de dados representando de um livro
+
+
+
