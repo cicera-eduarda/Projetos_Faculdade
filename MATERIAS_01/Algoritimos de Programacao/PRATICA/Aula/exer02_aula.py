a8a4cb52c2288ec16333f93eec310d83c230494f
cissa-eduarda <ciceraeduardac@gmail.com>
2023-06-14 15:39:51 -0300

    Organizacao Geral semestre1

--- MATERIAS_01/Algoritimos de Programacao/PRATICA/Aula/exer02_aula.py
+++
@@ -1,5 +0,0 @@
-# Leitura de Dados
-
-c=input("\n\n\nDigite uma frase: ")
-
-print(f"\n \t => c = {c:10}") #tabulacao e pulada de linha da variavel c
--- MATERIAS_01/Algoritimos de Programacao/PRATICA/Aula/exer07_aula.py
+++
@@ -1,6 +0,0 @@
-#Calcule o volume do cubo
-
-lado=float(input("insira o lado do cubo "))
-cubo=lado**3
-
-print(f"O volume do cubo de lado {lado} é {cubo}")
